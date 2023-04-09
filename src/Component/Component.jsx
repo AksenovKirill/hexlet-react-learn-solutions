39b00835890f9ec65d3d4069a10bbe58719e89e5
Kirill_Aksenov <k.aksenov@ians.aero>
2023-04-09 22:42:07 +0300

    added worked solution

--- src/Component/Component.jsx
+++ src/Component/Component.jsx
@@ -1,4 +1,3 @@
-import get from "lodash/get";
 import uniqueId from "lodash/uniqueId";
 import React from "react";
 
@@ -11,33 +10,39 @@ export default class Component extends React.Component {
     };
   }
   handleIncrement = () => {
-    this.setState((state) => ({ value: state.value + 1 }));
     this.setState((state) => ({
+      value: state.values.length ? state.value : 0,
+    }));
+    this.setState((state) => ({
+      value: (state.value += 1),
       values: [...state.values, { value: state.value, id: uniqueId() }],
     }));
   };
 
   handleDecrement = () => {
-    this.setState((state) => ({ value: state.value - 1 }));
-    this.setState((state) => ({
-      values: [...state.values, { value: state.value, id: uniqueId() }],
-    }));
+    if (this.state.value !== 0) {
+      this.setState((state) => ({
+        value: (state.value -= 1),
+        values: [...state.values, { value: state.value, id: uniqueId() }],
+      }));
+    }
   };
 
-  handleDelete = (value) => (event) => {
+  handleDelete = (id) => (event) => {
     event.preventDefault();
-    console.log(typeof value);
+
     this.setState((state) => ({
-      values: state.values.filter((elem) => value !== elem.value),
+      values: state.values.filter((elem) => id !== elem.id),
     }));
   };
   render() {
     const { values } = this.state;
+    console.log(this.state);
     const items = values
       .map((item) => (
         <button
           key={item.id}
-          onClick={this.handleDelete(item.value)}
+          onClick={this.handleDelete(item.id)}
           type="button"
           className="list-group-item list-group-item-action"
         >
@@ -64,9 +69,7 @@ export default class Component extends React.Component {
             -
           </button>
         </div>
-        {!!this.state.values.length && (
-          <div className="list-group">{items}</div>
-        )}
+        {!!values.length && <div className="list-group">{items}</div>}
       </div>
     );
   }
